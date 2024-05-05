a96d3f9107996302f8759b55d728c9f9e434ba21
namnameeroo <nami.skageec@gmail.com>
2024-05-05 15:32:59 +0900

    Delete: remove footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Footer from "@/app/_components/footer";
 import {HOME_OG_IMAGE_URL} from "@/lib/constants";
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
@@ -57,7 +56,6 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <div className="min-h-screen">{children}</div>
-        <Footer />
       </body>
     </html>
   );
